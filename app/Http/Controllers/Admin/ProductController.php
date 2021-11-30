1fe67577f9e2a88a51a25031373a3d42e9f60923
Muhammad Aiman <aimanabdollah@gmail.com>
2021-11-30 10:56:24 +0800

    Modify Product Controller

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
@@ -21,7 +22,7 @@ class ProductController extends Controller
         return view('admin.product.add', compact('category'));
     }
 
-     public function insert(Request $request)
+    public function insert(Request $request)
     {
         $products = new Product();
          if($request->hasFile('image'))
@@ -34,24 +35,88 @@ class ProductController extends Controller
 
         }
         
-          $products->cate_id = $request->input('cate_id');
-          $products->name = $request->input('name');
-          $products->slug = $request->input('slug');
-          $products->small_description = $request->input('small_description');
-          $products->description = $request->input('description');
-          $products->original_price = $request->input('original_price');
-          $products->selling_price = $request->input('selling_price');
-          $products->tax = $request->input('tax');
-          $products->qty = $request->input('qty');
-          $products->status = $request->input('status') == TRUE? '1':'0';
-          $products->popular = $request->input('popular') == TRUE? '1':'0';
-          $products->meta_title = $request->input('meta_title');
-          $products->meta_keywords = $request->input('meta_keywords');
-          $products->meta_description = $request->input('meta_description');
-          $products->save();
+        $products->cate_id = $request->input('cate_id');
+        $products->name = $request->input('name');
+        $products->slug = $request->input('slug');
+        $products->small_description = $request->input('small_description');
+        $products->description = $request->input('description');
+        $products->original_price = $request->input('original_price');
+        $products->selling_price = $request->input('selling_price');
+        $products->tax = $request->input('tax');
+        $products->qty = $request->input('qty');
+        $products->status = $request->input('status') == TRUE? '1':'0';
+        $products->popular = $request->input('popular') == TRUE? '1':'0';
+        $products->meta_title = $request->input('meta_title');
+        $products->meta_keywords = $request->input('meta_keywords');
+        $products->meta_description = $request->input('meta_description');
+        $products->save();
         return redirect('/products')->with('status', "Product Added Successfully");
+    }
 
-       
+    public function edit($id)
+    {
+        $products = Product::find($id);
+        return view('admin.product.edit', compact('products'));
+        //  $category = Category::all();
+        // return view('admin.product.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id ) 
+    {
+
+        
+           
+            $products = Product::find($id);
+            if($request->hasFile('image'))
+            {
+                $path = 'assets/uploads/products/'.$products->image;
+                if(File::exists($path))
+                {
+                    File::delete($path);
+
+                }
+                $file = $request->file('image');
+                $ext = $file->getClientOriginalExtension();
+                $filename = time(). '.'.$ext;
+                $file->move('assets/uploads/products',$filename);
+                $products->image = $filename;
+            }
+
+    
+            // $products->cate_id = $request->input('cate_id');
+            $products->name = $request->input('name');
+            $products->slug = $request->input('slug');
+            $products->small_description = $request->input('small_description');
+            $products->description = $request->input('description');
+            $products->original_price = $request->input('original_price');
+            $products->selling_price = $request->input('selling_price');
+            $products->tax = $request->input('tax');
+            $products->qty = $request->input('qty');
+            $products->status = $request->input('status') == TRUE? '1':'0';
+            $products->popular = $request->input('popular') == TRUE? '1':'0';
+            $products->meta_title = $request->input('meta_title');
+            $products->meta_keywords = $request->input('meta_keywords');
+            $products->meta_description = $request->input('meta_description');
+            $products->update();
+            return redirect('products')->with('status', "Product Updated Successfully");
+    }
+
+    public function destroy($id) 
+
+    {
+            $products = Product::find($id);
+            if($products->image)
+            {
+                $path = 'assests/uploads/products/'.$products->image;
+                if(File::exists($path))
+                {
+                    File::delete($path);
+                }
+                  $products->delete();
+                return redirect('products')->with('status', "Product Deleted Successfully");
+
+            }
+
     }
 
 }
